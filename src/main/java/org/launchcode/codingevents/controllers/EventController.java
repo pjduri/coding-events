e0a25d4b0e4cddefaaf4c17bfe63bdc8923ca5b9
Your Name <pjduri@gmail.com>
2022-10-06 17:06:22 -0500

    created display and process methods for editing event names/descriptions (included if statement to not change anything if field is empty), added edit.html, updated the index template to include edit links with each event in the table

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -6,9 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 @RequestMapping("events")
 public class EventController {
@@ -46,6 +43,29 @@ public class EventController {
             for (int id : eventIds) {
                 EventData.remove(id);
             }
+        }
+        return "redirect:";
+    }
+
+    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId) {
+        Event eventToEdit = EventData.getByID(eventId);
+        String title = "Edit Event " + eventToEdit.getName() + " (id=" + eventToEdit.getId() + ")";
+        model.addAttribute("title", title);
+        model.addAttribute("event", eventToEdit);
+        return "events/edit";
+    }
+
+    @PostMapping("edit")
+    public String processEditForm (@RequestParam int eventId,
+                                   @RequestParam String name,
+                                   @RequestParam String description) {
+        Event eventToEdit = EventData.getByID(eventId);
+        if (!name.equals(null) && !name.equals("")) {
+            eventToEdit.setName(name);
+        }
+        if (!description.equals(null) && !description.equals("")) {
+            eventToEdit.setDescription(description);
         }
         return "redirect:";
     }
