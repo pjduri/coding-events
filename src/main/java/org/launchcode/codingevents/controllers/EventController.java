cd5de08f6170cde7b78de8e300e4f33ecc0090fb
Your Name <pjduri@gmail.com>
2022-10-06 15:44:58 -0500

    used model binding to have the form interact directly with the event class. also fixed the hard html coding of create and delete page titles to come from the associated display methods.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,10 +4,7 @@ import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,15 +21,15 @@ public class EventController {
 
     // lives at /events/create
     @GetMapping("create")
-    public String processCreateEventForm() {
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
     // lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName,
-                              @RequestParam String eventDescription) {
-        EventData.add(new Event (eventName, eventDescription));
+    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+        EventData.add(newEvent);
         return "redirect:";
     }
 
@@ -44,7 +41,7 @@ public class EventController {
     }
 
     @PostMapping("delete")
-    public String processdeleteEventForm(@RequestParam(required = false) int[] eventIds) {
+    public String processDeleteEventForm(@RequestParam(required = false) int[] eventIds) {
         if (eventIds != null) {
             for (int id : eventIds) {
                 EventData.remove(id);
