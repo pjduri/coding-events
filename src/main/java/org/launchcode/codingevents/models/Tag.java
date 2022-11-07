2c4c09aeb52687f3a795975284b53f4220ebb3a9
Your Name <pjduri@gmail.com>
2022-11-06 20:29:15 -0600

    added List fields in Event and Tag with ManytoMany annotation.

--- src/main/java/org/launchcode/codingevents/models/Tag.java
+++ src/main/java/org/launchcode/codingevents/models/Tag.java
@@ -1,8 +1,11 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Tag extends AbstractEntity {
@@ -10,6 +13,9 @@ public class Tag extends AbstractEntity {
     @NotBlank(message = "Name is required.")
     @Size(min = 3, max = 50, message = "Name must between 3 and 50 characters.")
     private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private final List<Event> events = new ArrayList<>();
 
     public Tag(String name) {
         this.name = name;
@@ -27,6 +33,10 @@ public class Tag extends AbstractEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Event> getEvents() {
+        return events;
     }
 
     @Override
