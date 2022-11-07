5ccaf84000119b78a98942ee1aa9ec56fa093008
Your Name <pjduri@gmail.com>
2022-11-07 11:22:55 -0600

    added logout method to AuthenticationController

--- src/main/java/org/launchcode/codingevents/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/codingevents/controllers/AuthenticationController.java
@@ -126,4 +126,10 @@ public class AuthenticationController {
         return "redirect:";
     }
 
+    @GetMapping("logout")
+    public String logout(HttpServletRequest request) {
+        request.getSession().invalidate();
+        return "redirect:login";
+    }
+
 }
